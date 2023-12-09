0b96512959fbd9f6dd0ab8999d3bad838ace4811
shezi443 <131624174+shezi443@users.noreply.github.com>
2023-12-09 16:04:04 +0500

    Update route.ts

--- app/api/create-intent/route.ts
+++ app/api/create-intent/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
+const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
   apiVersion: "2023-10-16",
   typescript: true,
 });
