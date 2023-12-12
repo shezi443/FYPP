ec2b0db242c673115e70ebd6c1b4c9917ca622b4
shezi443 <131624174+shezi443@users.noreply.github.com>
2023-12-12 16:37:08 +0500

    Update route.ts

--- app/api/reservations/[reservationId] /route.ts
+++ app/api/reservations/[reservationId] /route.ts
@@ -1,31 +1,37 @@
-
 import { NextResponse } from "next/server";
-import prisma from '@/app/libs/prismadb'
 
-export async function POST(request:Request){
-    const body = await request.json();
-    const {
-        listingId,
-        startDate,
-        userId,
-        endDate,
-        totalPrice,
-        pet,
-        child,
-        adult
-    } = body;
-    // create reservations
-    const reservations = await prisma.reservation.create({
-        data:{
-            listingId,
-            startDate,
-            endDate,
-            userId,
-            totalPrice,
-            pet,
-            child,
-            adult
-        }
-    })
-    return NextResponse.json(reservations)
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+  reservationId?: string;
+}
+
+export async function DELETE(
+  request: Request, 
+  { params }: { params: IParams }
+) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const { reservationId } = params;
+
+  if (!reservationId || typeof reservationId !== 'string') {
+    throw new Error('Invalid ID');
+  }
+
+  const reservation = await prisma.reservation.deleteMany({
+    where: {
+      id: reservationId,
+      OR: [
+        { userId: currentUser.id },
+        { listing: { userId: currentUser.id } }
+      ]
+    }
+  });
+
+  return NextResponse.json(reservation);
 }
